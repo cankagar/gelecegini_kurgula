cdbb94a70cfd26785b86004110b7b18fc1e1ab3f
Enes Malik Arı <enesmalik1245@gmail.com>
2026-07-26 18:34:09 +0300

    refactor(api): remove unused server API files and clean up configuration

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Forced restart to load .env variables and Prisma Client
 };
 
 export default nextConfig;
- 
